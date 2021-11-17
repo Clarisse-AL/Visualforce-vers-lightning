0756ec711dfc157182da522c5b970a044ee5e0f8
clarisse <clarissealamelu@gmail.com>
2021-11-17 17:29:34 +0100

    Migration Ok

--- Egmanufacture backup/force-app/main/default/aura/OpptSearchPageNew/OpptSearchPageNewHelper.js
+++ Egmanufacture backup/force-app/main/default/aura/OpptSearchPageNew/OpptSearchPageNewHelper.js
@@ -20,4 +20,36 @@
         });
         $A.enqueueAction(action);
     }, 
+    
+    getOpportunitiesListWithKeyword: function (component, event, helper) {
+        
+        
+        var searchValue = component.get("v.searchValue");
+        console.log("searchValue: " + searchValue);
+        
+        var recordId = component.get("v.recordId");
+        console.log("recordId: "+recordId);
+        
+        var action = component.get("c.getOpportunitiesListWithKeyword");
+        action.setParams({ accountId : recordId,
+                           keyword : searchValue 
+                         });
+        console.log("ok1");
+        
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            console.log(response.getReturnValue().length);
+            if(state === 'SUCCESS'){
+                var opportunities = response.getReturnValue();
+                console.log("opportunities from server with keyword: "+ JSON.stringify(opportunities));
+                component.set("v.myData", opportunities);
+            }
+            else {
+                console.log("Errors: " + JSON.stringify(error));
+            }
+        });
+        $A.enqueueAction(action);
+        
+    }
+    
 })
